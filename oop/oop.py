bbb53eec03f2d4e2bfc939966355e0e60ce8f0a1
Oleksii Korolov <oleksii.korolov@harmonicinc.com>
2017-09-04 12:00:42 +0300

    1st version

---
+++ oop/oop.py
@@ -0,0 +1,67 @@
+# Defining class department
+class Department:
+    def __init__(self, *managers):
+        self.managers = managers
+
+    def give_salary(self):
+        for each in self.managers:
+            for person in each.list_employees:
+                print (person.first_name+' '+person.second_name+': got salary '+ str(person.get_salary()))
+        for manager in self.managers:
+            print(manager.first_name + ' ' + manager.second_name + ': got salary ' + str(manager.get_salary()))
+
+# Defining the Employee class
+class Employee:
+    def __init__(self, first_name, second_name, salary, experience, manager):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.salary = salary
+        self.experience = experience
+        self.manager = manager
+
+    def __repr__(self):
+        return self.first_name + ' ' + self.second_name + ' manager: ' + self.manager.first_name + ' ' + self.manager.second_name + ' experience: ' + str(self.experience)
+
+    def get_salary(self):
+        if self.experience > 5: self.salary=self.salary*1.2+500
+        elif self.experience > 2: self.salary+=200
+        return self.salary
+
+# Class Developer inherited from Employee
+class Developer(Employee):
+    pass
+
+# Class Designer inherited from Employee
+class Designer(Employee):
+    def __init__(self, first_name, second_name, salary, experience, manager, coefficient):
+        super().__init__(first_name, second_name, salary, experience, manager)
+        self.coefficient = coefficient
+
+    def get_salary(self):
+        return self.salary*self.coefficient
+
+# Class Manager inherited from Employee
+class Manager(Employee):
+    def __init__(self, first_name, second_name, salary, experience, manager, list_employees = None):
+        super().__init__(first_name, second_name, salary, experience, manager)
+        if list_employees is None:
+            self.list_employees = []
+        else:
+            self.list_employees = list_employees
+
+    def get_salary(self):
+        if len(self.list_employees)>10:self.salary+=500
+        elif len(self.list_employees)>5:self.salary+=200
+        if len(list([lambda empl: type(empl) is Developer, self.list_employees])) > len(self.list_employees) / 2:self.salary=self.salary*1.1
+        return self.salary
+
+Bruce = Manager('Bruce', 'Wayne', 10000, 4, None, None)
+Diana = Developer('Diana', 'Prince', 5000, 6, Bruce)
+Frank = Designer('Mark', 'Rufalo', 600, 3, 'Tony', 10)
+Tony = Manager('Tony', 'Stark', 9000, 3, None, [Frank])
+Bruce = Manager('Bruce', 'Wayne', 10000, 4, None, [Diana])
+
+myDep = Department(Bruce, Tony)
+myDep.give_salary()
+
+print(Diana)
